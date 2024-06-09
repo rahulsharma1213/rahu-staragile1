ee7baaeae315d8455000076ced8d361d341c62a5
rahul <rahu@gmail.com>
2024-06-09 17:09:56 +0530

    first commit

---
+++ src/main/java/raghu_demo/raghu_demo/RaghuDemoApplication.java
@@ -0,0 +1,13 @@
+package raghu_demo.raghu_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaghuDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaghuDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/raghu_demo/raghu_demo/RaghuDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package raghu_demo.raghu_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaghuDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
